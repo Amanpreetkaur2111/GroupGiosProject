b88974614c835631c0a0bef7d75926e59493860c
Amanpreetkaur <aman021011@gmail.com>
2019-11-18 15:00:24 -0500

    initial commit

---
+++ GroupGiosProject/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  GroupGiosProject
+//
+//  Created by MacStudent on 2019-11-18.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        assignbackground()
+    }
+     func assignbackground(){
+        let background = UIImage(named: "hotel pic")
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
+}
+
