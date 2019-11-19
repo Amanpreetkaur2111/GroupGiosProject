ac858d82fc48575bc6e4712d8311a2c8597186ae
Amanpreetkaur <aman021011@gmail.com>
2019-11-19 18:59:47 -0500

    shows room types

---
+++ GroupGiosProject/roomstructure.swift
@@ -0,0 +1,12 @@
+//
+//  roomstructure.swift
+//  GroupGiosProject
+//
+//  Created by MacStudent on 2019-11-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+struct rooms {
+    static var roomTypes = ["Single","Double","Cabana","Stereo"]
+}
